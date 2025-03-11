f74d44beef6db9d0da876592262e511e0fdc0b09
Matias <matiasezequielsmania@gmail.com>
2025-03-11 19:25:21 -0300

    .

--- Front/src/components/custom/Writing.jsx
+++ Front/src/components/custom/Writing.jsx
@@ -5,7 +5,7 @@ import { Award } from 'lucide-react'; // Importar el icono de medalla
 function Writing({ title, description, date, url, ranking }) {
     return (
         <a href={url} target="_blank" rel="noopener noreferrer">
-            <div className="mx-auto p-6 m-4 rounded-xl shadow-2xl bg-white dark:bg-[#1f0148] text-gray-800 dark:text-white transform transition duration-300 ease-in-out hover:scale-105 relative space-y-4">
+            <div className="mx-auto p-6 m-4 rounded-xl shadow-2xl bg-white dark:bg-[#1f0148] text-gray-800 dark:text-white transform transition duration-300 ease-in-out hover:scale-105 relative space-y-2">
                 <h2 className="text-xl font-semibold text-gray-800 dark:text-white">{title}</h2>
                 <p className="text-gray-600 dark:text-gray-300 mt-2">{description}</p>
                 <p className="text-xs text-gray-500 dark:text-gray-400 mt-3">Publicado en: {date}</p>
